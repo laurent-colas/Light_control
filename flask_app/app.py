a5c1465b34a3e2593398157533c80ac49d5c166a
Laurent Colas <laurent@colas.ca>
2018-07-27 15:16:56 -0400

    Done more work on it. added mutliple buttons, with state and adresse all together

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, render_template
 from collections import defaultdict
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import FloatField
 
 import comm_functions as comm
 import adresse_lib as address_lib
+
+'''
 import RPi.GPIO as GPIO
 import smbus
 
@@ -14,6 +16,7 @@ bus = smbus.SMBus(1)
 address = 0x04
 GPIO.setmode(GPIO.BCM)
 
+'''
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'you-will-never-guess'
@@ -46,36 +49,74 @@ def ButtonHandler():
     return 'Button pressed by ESP8266'
 
 
-@app.route('/')
+@app.route('/hello_world')
 def hello_world():
     return 'Hello World!'
 
 
-@app.route('/button', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def divide():
 
-    class DivideForm(Form):
+    class DivideForm(FlaskForm):
         numerator = FloatField("Number")
         denominator = FloatField("Divide by")
 
     form = DivideForm()
     result = None
 
+    list_places =address_lib.places_name
+
+
+
     if form.validate_on_submit():
-        but_val = request.form['submit']
-        targetPins = address_lib.button_address[but_val]
+        #   receive
+        light_address = request.form['submit']
+        # light_address = but_val
 
-        comm.simulate_send_address(targetPins, 1)
+        # light_address_name = request.form['place']
 
-        if but_val == "B1":
-            result = 1
-        elif but_val == "B2":
-            result = 2
-        else:
-            result = 2
+        # light_address = but_val[0:2]
+        # light_state = but_val[2:]
+        # test = but_val[3:]
 
-    return render_template('button.html', result=result, form=form)
+        light_state = 3
+
+        for key in list_places.keys():
+            if light_address == key:
+                target_pins = list_places[key]
+                address_key = key
+                if target_pins[0] == 1:
+                    list_places[key][0] = 0
+                else:
+                    list_places[key][0] = 1
+
+                #
+                # if list_places[key] == 1:
+                #     list_places[key] = 0
+                #     light_state = 0
+                # else:
+                #     list_places[key] = 1
+                #     light_state = 1
+
+        result = 3
+        # targetPins = address_lib.button_address[light_address]
+        final_target_pins = list_places[light_address][1:]
+        final_state = list_places[light_address][0]
+
+        comm.simulate_send_address(final_target_pins, final_state)
+        # if light_address == "B1":
+        #     result = 1
+        # elif light_address == "B2":
+        #     result = 2
+        # else:
+        #     result = 3
+
+
+
+
+    return render_template('button.html',list_places=list_places, form=form)
 
 
 if __name__ == '__main__':
     app.run()
+
